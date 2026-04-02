16624b0c6338cd520e32bdbd4b4f79e7767886e6
SASMacBookPro14 <hirokisasazawa@SASMacBookPro14MacBook-Pro.local>
2026-04-02 20:20:03 +0900

    update site 2026-04-02 20:20:03

--- quartz/components/FeedbackForm.tsx
+++ quartz/components/FeedbackForm.tsx
@@ -1,28 +1,27 @@
-
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const FeedbackForm: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
-  if (!fileData.frontmatter?.feedback_form) {
-    return null
-  }
+  if (!fileData.frontmatter?.feedback_form) {
+    return null
+  }
 
-  return (
-    <div style={{ marginTop: "3rem" }}>
-      <h2>フィードバックフォーム</h2>
-      <p>
-        誤記指摘、改善提案、追加希望、質問などがあれば以下からお寄せください。
-        医療相談、患者個別相談には対応していません。個人情報は記載しないでください。
-      </p>
-      <iframe
-        src="<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJmx5Zyd6SqLpBbCV2MjZmxmP-xtDnYBxYPklMr5D7tv0nlQ/viewform?embedded=true" width="640" height="1772" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>"
-        width="100%"
-        height="1100"
-        style={{ border: "none" }}
-      >
-        読み込んでいます…
-      </iframe>
-    </div>
-  )
+  return (
+    <div style={{ marginTop: "3rem" }}>
+      <h2>フィードバックフォーム</h2>
+      <p>
+        誤記指摘、改善提案、追加希望、質問などがあれば以下からお寄せください。
+        医療相談、患者個別相談には対応していません。個人情報は記載しないでください。
+      </p>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScJmx5Zyd6SqLpBbCV2MjZmxmP-xtDnYBxYPklMr5D7tv0nlQ/viewform?embedded=true"
+        width="100%"
+        height="1772"
+        style={{ border: "none" }}
+      >
+        読み込んでいます…
+      </iframe>
+    </div>
+  )
 }
 
 export default (() => FeedbackForm) satisfies QuartzComponentConstructor
